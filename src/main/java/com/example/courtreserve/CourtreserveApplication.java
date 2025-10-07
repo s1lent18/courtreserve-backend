6731febf4f7bd39575c96c7dd33bc75f5935bcd8
ali <alijafar.rajani@gmail.com>
2025-10-07 07:18:41 +0500

    Initial commit

---
+++ src/main/java/com/example/courtreserve/CourtreserveApplication.java
@@ -0,0 +1,13 @@
+package com.example.courtreserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourtreserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourtreserveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/courtreserve/CourtreserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courtreserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourtreserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
